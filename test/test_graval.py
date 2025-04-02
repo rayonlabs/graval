a51603e028510ddad76d3e4eea441c5c1ef446b6
Jon Durbin <jon@jondurbin.com>
2025-04-02 08:56:39 -0400

    OpenCL/clBlast version, WIP

--- test/test_graval.py
+++ test/test_graval.py
@@ -14,22 +14,22 @@ def test_encrypt_decrypt():
     """
     miner = Miner()
     validator = Validator()
-    validator.initialize()
 
     # Validator generates a unique seed.
     seed = random.randint(0, 100000)
 
     # Initialize the miner node.
-    _ = miner.initialize(seed)
+    miner.initialize(seed)
     device_info = miner.get_device_info(0)
-    print(f"Device info: {device_info} {device_info.__class__}")
+    print(f"Miner device info: {device_info}")
 
     # Encrypt as the validator.
     plaintext = "Testing a super secret message..."
     ciphertext, iv, length = validator.encrypt(device_info, plaintext, seed)
     print(f"Encrypted (validator): '{plaintext}' -- {length} bytes ciphertext")
+    print(f"{ciphertext.hex()} {iv.hex()} {length=}")
 
-    # Decrypt as the miner.
+    ## Decrypt as the miner.
     decrypted = miner.decrypt(ciphertext, iv, length, 0)
     print(f"Decrypted (miner):     '{decrypted}'")
     assert decrypted == plaintext, f"'{decrypted}' vs '{plaintext}'"
@@ -43,14 +43,16 @@ def test_encrypt_decrypt():
     assert plaintext == decrypted
     print("Successfully verified encryption/decryption")
 
+    miner.shutdown()
+    validator.shutdown()
+
 
 def test_matrix_challenge():
     miner = Miner()
     validator = Validator()
-    validator.initialize()
 
     # Initialize, but really only so we can get the device info.
-    _ = miner.initialize(42)
+    _ = miner.initialize(42, 1)
     device_info = miner.get_device_info(0)
 
     # Generate a random string, which we'll encrypt with a random seed.
@@ -64,19 +66,22 @@ def test_matrix_challenge():
     miner_response = miner.process_matrix_challenge(seed, ciphertext, iv, length)
     assert miner_response == challenge_string
     print(f"Successfully responded to challenge string: {miner_response}")
+    miner.shutdown()
+    validator.shutdown()
 
 
 def test_device_info_challenge():
     miner = Miner()
-    device_count = miner.initialize(42)
+    device_count = miner._device_count
     devices = [miner.get_device_info(idx) for idx in range(device_count)]
     validator = Validator()
-    validator.initialize()
     for _ in range(200):
         challenge = validator.generate_device_info_challenge(device_count)
         response = miner.process_device_info_challenge(challenge)
         assert validator.verify_device_info_challenge(challenge, response, devices)
     print("Successfully verified 200 device info challenges")
+    miner.shutdown()
+    validator.shutdown()
 
 
 def test_filesystem_challenge():
@@ -90,9 +95,7 @@ def test_filesystem_challenge():
         expected = hashlib.sha256(
             file_bytes[i * chunk_size : i * chunk_size + chunk_size]
         ).hexdigest()
-        miner_output = miner.process_filesystem_challenge(
-            path, i * chunk_size, chunk_size
-        )
+        miner_output = miner.process_filesystem_challenge(path, i * chunk_size, chunk_size)
         assert expected == miner_output
     print(f"Successfully verified {int(size / chunk_size)} filesystem challenges.")
 
@@ -107,3 +110,4 @@ def test_filesystem_challenge():
         miner.process_filesystem_challenge(path, 2 * 10**8, 4)
     with pytest.raises(GraValError):
         miner.process_filesystem_challenge(path, 0, 0)
+    miner.shutdown()
